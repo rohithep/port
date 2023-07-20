6c89a0aef522ee6ba122cea23f263efac8118b80
Rohith ep <rohithep2002@gmail.com>
2023-07-20 21:15:24 +0530

    second commit

--- app.js
+++ app.js
@@ -1,39 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
-// Set EJS as the templating engine
 app.set('view engine', 'ejs');
-
-// Set the path to the 'views' folder where EJS templates will be stored
 app.set('views', path.join(__dirname, 'views'));
-
-// Serve static files from the 'public' folder (replace 'public' with the name of your static files folder)
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Your other routes and configurations go here
-
-// ...
-
-
-// Your other routes and configurations go here
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-// Define the home page route
+
 app.get('/', (req, res) => {
   const data = {
     title: 'Home',
-    yourName: 'John Doe',
-    description: 'Web Developer', // Add a description here
-    currentYear: new Date().getFullYear(), // To show the current year in the footer
-    // Add other data you want to display on the home page
+    yourName: 'SREETHI',
+    description: 'ARTIST', 
+    currentYear: new Date().getFullYear(), 
+    
   };
 
   res.render('index', data);
 });
 
-const port = 3000; // You can change this to any port you prefer
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
